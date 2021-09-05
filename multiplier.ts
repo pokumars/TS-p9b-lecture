e3b9f53d9f570babff09fa92db0bdbfdcc8ed8ee
Oheneba Poku-Marboah <ohenebakob@hotmail.com>
2021-09-05 11:32:56 +0300

    9b (Creating your first own types) - multiplier

--- multiplier.ts
+++ multiplier.ts
@@ -1,5 +1,29 @@
-const multiplicator = (a: number, b: number, printText: string) => {
-  console.log(printText, a * b);
+type Operation = "multiply" | "add" | "divide";
+type Result = number;
+
+const multiplicator = (a: number, b: number, op: Operation): Result => {
+  switch (op) {
+    case "multiply":
+      return a * b;
+
+    case "add":
+      return a + b;
+
+    case "divide":
+      if (b === 0) throw new Error('can\'t divide by 0');
+      return a / b;
+
+    default:
+      throw new Error('Operation is not multiply, add or divide!');
+  }
 }
 
-multiplicator(2, 4, 'Multiplied numbers 2 and 4, the result is:');
+
+
+try {
+  //console.log(multiplicator(2, 4, 'add'));
+  console.log(process.argv)
+
+} catch (e) {
+  console.log('Something went wrong, error message: ', e.message);
+}
