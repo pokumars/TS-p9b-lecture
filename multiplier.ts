516f7dd90e7070ed253cf6ef4ae423677631801d
Oheneba Poku-Marboah <ohenebakob@hotmail.com>
2021-09-06 05:27:59 +0300

    9b- (Improving the project) add an argument parser with good error messages

--- multiplier.ts
+++ multiplier.ts
@@ -1,10 +1,33 @@
-const multiplicator = (a: number, b: number, printText: string) => {
-  console.log(printText,  a * b);
+interface MultiplyValues {
+  value1: number;
+  value2: number;
 }
 
-const a: number = Number(process.argv[2])
-const b: number = Number(process.argv[3])
-multiplicator(a, b, `Multiplied ${a} and ${b}, the result is:`);
+const parseArguments = (args: Array<string>): MultiplyValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      value1: Number(args[2]),
+      value2: Number(args[3])
+    }
+  } else {
+    throw new Error("Provided values were not numbers!");
+  }
+}
+
+const multiplicator = (a: number, b: number, printText: string) => {
+  console.log(printText, a * b);
+}
+
+try {
+  const { value1, value2 } = parseArguments(process.argv);
+
+  multiplicator(value1, value2, `Multiplied ${value1} and ${value2}, the result is:`);
+} catch (e) {
+  console.log('Error, something bad happened, message: ', e.message);
+}
 
 
 /* v1 before https://fullstackopen.com/en/part9/first_steps_with_type_script#improving-the-project
